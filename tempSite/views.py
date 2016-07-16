853f069bef6bcaaea084751bd81b9588220be2f9
Simon Jones <spjones1981@gmail.com>
2016-07-16 16:29:38 +0100

    templated the old site and added a test page

--- tempSite/views.py
+++ tempSite/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
-	return render(request, 'tempSite/index.html') 
+#	return render(request, 'tempSite/index.html') # main static old site, templated but with no link to the test page
+	return render(request, 'tempSite/index_cp.html') # static old site with link to test page in fbj logo
+
+def test(request):
+	return render(request, 'tempSite/test_page.html')
